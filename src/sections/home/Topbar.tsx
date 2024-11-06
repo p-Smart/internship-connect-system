59787c42aa85bf63ffa0885ce6c3b9dd81b88196
Prince <princeayokunle2002@gmail.com>
2024-11-07 00:26:43 +0100

    feat(home): add logo

--- src/sections/home/Topbar.tsx
+++ src/sections/home/Topbar.tsx
@@ -1,7 +1,28 @@
+import Logo from "@/components/Logo";
 import { Flex } from "@chakra-ui/react";
 
 const Topbar = () => {
-  return <Flex></Flex>;
+  const nav = [
+    {
+      title: "Companies",
+      router: "",
+    },
+    {
+      title: "Interns",
+      router: "",
+    },
+    {
+      title: "About us",
+      router: "",
+    },
+  ];
+
+  return (
+    <Flex>
+      <Logo variant="light" />
+      <Flex></Flex>
+    </Flex>
+  );
 };
 
 export default Topbar;
